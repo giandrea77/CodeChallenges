20a083804414ef6f591d9be5210be119e5025a28
Andrea <andrea.girardi@gmail.com>
2021-08-25 19:43:26 +0200

    Euler challenge: Day #2

--- HackerRank/src/main/java/it/ag/euler/ProjectEuler.java
+++ HackerRank/src/main/java/it/ag/euler/ProjectEuler.java
@@ -7,39 +7,6 @@ public class ProjectEuler {
 
 
     /**
-     * Project Euler #4: Largest palindrome product
-     * https://www.hackerrank.com/contests/projecteuler/challenges/euler004/problem
-     *
-     * Q: Find the largest palindrome made from the product of two 3-digit numbers which is less than
-     */
-    private static boolean isPalindrome(int number) {
-
-        char[] arr = String.valueOf(number).toCharArray();
-        int size = arr.length;
-        for (int index = 0; index > size / 2; index ++) {
-
-            if ( arr[index] != arr[size - index] ) {
-                return false;
-            }
-
-        }
-
-        return true;
-
-    }
-
-    private static int largestPalindromeProduct(int number) {
-
-        if ( isPalindrome(number) ) {
-            System.out.println("");
-        }
-
-        return 0;
-
-    }
-
-
-    /**
      * Project Euler #3: Largest prime factor
      * https://www.hackerrank.com/contests/projecteuler/challenges/euler003/problem
      *
@@ -83,8 +50,138 @@ public class ProjectEuler {
 
     }
 
+    /**
+     * Project Euler #4: Largest palindrome product
+     * https://www.hackerrank.com/contests/projecteuler/challenges/euler004/problem
+     *
+     * Q: Find the largest palindrome made from the product of two 3-digit numbers which is less than
+     */
+    private static boolean isPalindrome(int number) {
+
+        char[] arr = String.valueOf(number).toCharArray();
+        int size = arr.length;
+        for (int index = 0; index < size / 2; index ++) {
+
+            if ( arr[index] != arr[(size - 1) - index] ) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+
+    private static int largestPalindromeProduct(int number) {
+
+        int smallest = 0;
+        if ( number < 101102 || number > 1000000 ) {
+            return 0;
+        }
+
+        for ( int index = 100; index <= 999; index++ ) {
+
+            for ( int innerIndex = 100; innerIndex <= 999; innerIndex++ ) {
+
+                int mult = index * innerIndex;
+                if ( mult > 101101 && isPalindrome(mult) && mult < number ){
+                    smallest = mult > smallest ? mult : smallest;
+                }
+
+            }
+
+
+        }
+
+        return smallest;
+
+    }
+
+    /**
+     * Project Euler #5: Smallest multiple
+     * https://www.hackerrank.com/contests/projecteuler/challenges/euler005/problem
+     *
+     * @param
+     */
+    public static int smallstMultiple(int number) {
+
+        if ( number == 1 ) {
+            return 1;
+        }
+
+        boolean found = false;
+        int result = 0;
+        while ( !found && result < 800000 ) {
+
+            result++;
+
+            for ( int index = 2; index <= number; index++ ) {
+
+                if ( result % index == 0 ) {
+                    found = false;
+                    break;
+                } else {
+                    found = true;
+                }
+
+            }
+        }
+
+        if (found) {
+            return result;
+        } else {
+            return 0;
+        }
+
+    }
+
+    /**
+     * Project Euler #6: Sum square difference
+     * https://www.hackerrank.com/contests/projecteuler/challenges/euler006/problem
+     *
+     * Find the absolute difference between the sum of the squares of the first  natural numbers and the square of the sum.
+     *
+     * @param
+     */
+    public static Long sumSquareDifference(Long number) {
+
+        long sumOfSquareEach = (number * (number + 1) * (2 * number + 1)) / 6;
+        long sumOfEachAndSquare = (long)Math.pow( (number * (number + 1)) / 2, 2);
+        return sumOfEachAndSquare - sumOfSquareEach;
+
+    }
+
+    /**
+     * Project Euler #7: 10001st prime
+     *
+     * @param number
+     */
+    public static int prime10001th(int number) {
+
+        return 0;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("getLargestPrimeFactor(17): " + getLargestPrimeFactor(44, 2));
+
+        // Project Euler #3: Largest prime factor
+        // System.out.println("getLargestPrimeFactor(17): " + getLargestPrimeFactor(44, 2));
+
+        // Project Euler #4: Largest palindrome product
+        // int result = largestPalindromeProduct(793397) ;
+        //if ( result > 0 ) {
+        //    System.out.println(result);
+        // }
+
+        // Project Euler #5: Smallest multiple
+        // System.out.println(smallstMultiple(32));
+
+        // Project Euler #6: Sum square difference
+        // System.out.println(sumSquareDifference(3L));
+
+        // Project Euler #7: 10001st prime
+        System.out.println(sumSquareDifference(3L));
+
     }
 
 }
